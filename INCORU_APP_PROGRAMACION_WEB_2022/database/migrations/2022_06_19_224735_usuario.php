59c0d0975026fc9d5f5d9ddbd0175642f00f8242
dflopezlo <dflopezlo@unal.edu.co>
2022-06-19 23:34:23 -0500

    se crea la migrsación de usuarios y la tabla usuario en la base de datos

---
+++ INCORU_APP_PROGRAMACION_WEB_2022/database/migrations/2022_06_19_224735_usuario.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Usuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->string('cedula', 10)->primary();
+            $table->string('nombreUsu', 15);
+            $table->string('celular', 10);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('direccion');
+            $table->date('fechaNac');
+            $table->string('rol', 15);
+            $table->string('contrasena');
+            $table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
